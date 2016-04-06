cd151a831119f6242eb9a2c6456a0d2c460065f7
mhunger <m.hunger@yahoo.de>
2016-04-07 00:44:13 +0200

    Added game class with started state and current score and connected it with basic view that has button and labels for the score. The game is initialised with a 0:0 score.

---
+++ tablesoccer-mobileapp/Game.swift
@@ -0,0 +1,38 @@
+//
+//  Game.swift
+//  tablesoccer-mobileapp
+//
+//  Created by Michael Hunger on 06/04/16.
+//  Copyright (c) 2016 Michael Hunger. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    private var started: Bool = false
+    
+    private var currentScore = [teamId:Int]()
+
+    enum teamId {
+        case team1
+        case team2
+    }
+    
+    func startGame() {
+        started = true
+        currentScore[teamId.team1] = 0
+        currentScore[teamId.team2] = 0
+    }
+    
+    func stopGame() {
+        started = false
+    }
+    
+    func isStarted() -> Bool {
+        return started
+    }
+    
+    func getTeamScore(team: teamId) -> Int{
+        return currentScore[team]!
+    }
+}
--- tablesoccer-mobileapp/ViewController.swift
+++ tablesoccer-mobileapp/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var game: Game = Game();
+    
+    @IBOutlet weak var teamOneScore: UILabel!
+    
+    @IBOutlet weak var teamTwoScore: UILabel!
+    
+    var teamOneScoreDisplayValue: Int{
+        get{
+            return NSNumberFormatter().numberFromString(teamOneScore.text!)!.integerValue
+        }
+        
+        set{
+            teamOneScore.text = "\(newValue)"
+        }
+    }
+    
+    var teamTwoScoreDisplayValue: Int{
+        get{
+            return NSNumberFormatter().numberFromString(teamTwoScore.text!)!.integerValue
+        }
+        
+        set{
+            teamTwoScore.text = "\(newValue)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +45,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func startStopGame(sender: UIButton, forEvent event: UIEvent) {
+        
+        if(game.isStarted()) {
+            game.stopGame()
+            sender.setTitle("Start Game", forState: UIControlState.Normal)
+        } else {
+            game.startGame()
+            sender.setTitle("Stop Game", forState: UIControlState.Normal)
+        }
+        
+        teamOneScoreDisplayValue = game.getTeamScore(Game.teamId.team1)
+        teamTwoScoreDisplayValue = game.getTeamScore(Game.teamId.team2)
+    }
 }
 
