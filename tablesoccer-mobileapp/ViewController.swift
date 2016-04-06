d859d7bf068bc51c75955b5be3cb019c273c06c2
mhunger <m.hunger@yahoo.de>
2016-04-06 22:29:19 +0200

    Initial Creation of soccermanager mobile app

---
+++ tablesoccer-mobileapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tablesoccer-mobileapp
+//
+//  Created by Michael Hunger on 06/04/16.
+//  Copyright (c) 2016 Michael Hunger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
