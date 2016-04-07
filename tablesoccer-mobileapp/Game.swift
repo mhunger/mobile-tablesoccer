c18f8012f0e4bde8bf9c218dd22c86e05813e4bb
mhunger <m.hunger@yahoo.de>
2016-04-07 22:28:11 +0200

    Added the match score. Implemented as two labels that get black background.
    Problems:
    - end of game not implemented
    - tests missing
    - score logic is heavily duplicated

--- tablesoccer-mobileapp/Game.swift
+++ tablesoccer-mobileapp/Game.swift
@@ -11,8 +11,17 @@ import Foundation
 class Game {
     private var started: Bool = false
     
+    private var finished: Bool = false
+    
     private var currentScore = [teamId:Int]()
+    private var matches = [teamId:Int]()
 
+    private var maxGoals: Int = 10
+    
+    private var winningScore: Int = 7
+    
+    private var maxMatches: Int = 2
+    
     enum teamId {
         case team1
         case team2
@@ -20,12 +29,19 @@ class Game {
     
     func startGame() {
         started = true
-        currentScore[teamId.team1] = 0
-        currentScore[teamId.team2] = 0
+        matches[teamId.team1] = 0
+        matches[teamId.team2] = 0
+        
+        startMatch()
     }
     
     func stopGame() {
         started = false
+    }
+    
+    func startMatch() {
+        currentScore[teamId.team1] = 0
+        currentScore[teamId.team2] = 0
     }
     
     func isStarted() -> Bool {
@@ -36,9 +52,38 @@ class Game {
         return currentScore[team]!
     }
     
-    func increaseScore(team: teamId) {
-        var tempScore: Int = currentScore[team]!;
+    func getTeamMatches() -> [teamId:Int]{
+        return matches
+    }
+    
+    func increaseScore(scorer: teamId, opponent: teamId) {
+        var tempScore: Int = currentScore[scorer]!
+        var tempOpponentScore: Int = currentScore[opponent]!
+        
         tempScore = (tempScore + 1)
-        currentScore[team] = tempScore
+        currentScore[scorer] = tempScore
+        
+        if(((tempScore >= winningScore)
+            && (tempOpponentScore < (tempScore - 1)))
+                || tempScore == maxGoals) {
+            finishMatch(scorer)
+        }
+    }
+    
+    func finishMatch(team: teamId) {
+        var matchesTemp = matches[team]! + 1
+        print("matchesTemp " + "\(matchesTemp)")
+        
+        matches[team] = matchesTemp
+        
+        if(matchesTemp >= maxMatches) {
+            finishGame()
+        } else {
+            startMatch()
+        }
+    }
+    
+    func finishGame(){
+        finished = true
     }
 }
--- tablesoccer-mobileapp/ViewController.swift
+++ tablesoccer-mobileapp/ViewController.swift
@@ -16,6 +16,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var teamTwoScore: UILabel!
     
+    @IBOutlet weak var teamOneMatchOne: UILabel!
+    
+    @IBOutlet weak var teamOneMatchTwo: UILabel!
+    
+    @IBOutlet weak var teamTwoMatchOne: UILabel!
+    
+    @IBOutlet weak var teamTwoMatchTwo: UILabel!
+    
     var teamOneScoreDisplayValue: Int{
         get{
             return NSNumberFormatter().numberFromString(teamOneScore.text!)!.integerValue
@@ -37,22 +45,38 @@ class ViewController: UIViewController {
     }
     
     @IBAction func increaseScoreTeamOnePlayerOne(sender: UIButton) {
-        game.increaseScore(Game.teamId.team1)
+        game.increaseScore(
+            Game.teamId.team1,
+            opponent: Game.teamId.team2
+        )
+        
         setTeamScore()
     }
     
     @IBAction func increaseScoreTeamOnePlayerTwo(sender: UIButton) {
-        game.increaseScore(Game.teamId.team1)
+        game.increaseScore(
+            Game.teamId.team1,
+            opponent: Game.teamId.team2
+        )
+        
         setTeamScore()
     }
     
     @IBAction func increaseScoreTeamTwoPlayerOne(sender: UIButton) {
-        game.increaseScore(Game.teamId.team2)
+        game.increaseScore(
+            Game.teamId.team2,
+            opponent: Game.teamId.team1
+        )
+        
         setTeamScore()
     }
     
     @IBAction func increaseScoreTeamTwoPlayerTwo(sender: UIButton) {
-        game.increaseScore(Game.teamId.team2)
+        game.increaseScore(
+            Game.teamId.team2,
+            opponent: Game.teamId.team1
+        )
+        
         setTeamScore()
     }
     
@@ -83,6 +107,32 @@ class ViewController: UIViewController {
         
         teamOneScoreDisplayValue = game.getTeamScore(Game.teamId.team1)
         teamTwoScoreDisplayValue = game.getTeamScore(Game.teamId.team2)
+        
+        setMatches()
+        
+    }
+    
+    func setMatches(){
+        var matches = game.getTeamMatches()
+        print(matches)
+        println()
+        
+        if(matches[Game.teamId.team1] > 0) {
+            teamOneMatchOne.backgroundColor = UIColor.blackColor()
+            print("writing color")
+        }
+        
+        if(matches[Game.teamId.team1] > 1) {
+            teamOneMatchTwo.backgroundColor = UIColor.blackColor()
+        }
+        
+        if(matches[Game.teamId.team2] > 0) {
+            teamTwoMatchOne.backgroundColor = UIColor.blackColor()
+        }
+        
+        if(matches[Game.teamId.team2] > 1) {
+            teamTwoMatchTwo.backgroundColor = UIColor.blackColor()
+        }
     }
 }
 
